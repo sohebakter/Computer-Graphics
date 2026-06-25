89cc50553704c9ca6c2201f7b72adf2d49ea34aa
SOHEB AKTER <sohebakter75@gmail.com>
2026-06-25 13:53:05 +0600

    LAB Tax 2

--- main.cpp
+++ main.cpp
@@ -1,65 +1,54 @@
+
+
 #include<windows.h>
 #include <GL/glut.h>
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    //This is for Point
-    glPointSize(10.0);
-	glBegin(GL_POINTS);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex2f(-0.5f, 0.5f);
-	glEnd();
-
-	 //This is for Line
-	glLineWidth(7.5);
+glLineWidth(10.1);
 	glBegin(GL_LINES);
-	glColor3ub(0, 0, 255);
-	glVertex2f(0.32f, 0.2f);
-	glVertex2f(0.6f, 0.8f);
-
-	glColor3ub(0, 255, 0);
-	glVertex2f(0.6f, 0.8f);
-	glVertex2f(0.22f, 0.7f);
-
 	glColor3ub(255, 0, 0);
-	glVertex2f(0.22f, 0.7f);
-	glVertex2f(0.32f, 0.2f);
+	glVertex2f(0.6f, -0.6f);
+	glVertex2f(-0.1f, 0.6f);
 	glEnd();
 
+	//just Tringles
+ glBegin(GL_TRIANGLES);
+ glColor3ub(0,0,0);
+ glVertex2d(-0.6,-0.6);
+ glVertex2d(-0.2,-0.6);
+ glVertex2d(-0.4,-0.1);
+ glEnd();
 
-	//This is for Triangles
+ glBegin(GL_QUADS);
+	glColor3ub(0,255,0);
+    glVertex2f(-0.8f, 0.2f);
+    glVertex2f(-0.5,0.2);
+    glVertex2f(-0.5f, 0.6f);
+    glVertex2f(-0.8,0.6);
+    glEnd();
+
 	glBegin(GL_TRIANGLES);
-	glColor3ub(255,0,0);
-	glVertex2f(-0.6f, -0.2f);
-	glVertex2f(-0.9f, -0.35f);
-	glVertex2f(-0.4f, -0.35f);
+	glColor3ub(255,255,0);
+	glVertex2f(-0.8f, 0.2f);
+	glVertex2f(-0.5f, 0.2f);
+	glVertex2f(-0.65f, 0.5f);
+	glEnd();
+
+glBegin (GL_POLYGON);
+	glColor3ub(0,155,255);
+    glVertex2f(0.3f, 0.4f);
+    glVertex2f(0.5f, 0.3f);
+    glVertex2f(0.7f, 0.4f);
+    glVertex2f(0.7f, 0.6f);
+    glVertex2f(0.5f, 0.7f);
+    glVertex2f(0.3f, 0.6f);
+
+
+
 	glEnd();
 
 
-	//This is for QUADS
-	glBegin(GL_QUADS);
-	glColor3ub(255,0,255);
-    glVertex2f(-0.3f, -0.9f);
-    glColor3ub(0,0,255);
-    glVertex2f(0.3f, -0.9f);
-    glColor3ub(255,0,0);
-    glVertex2f(0.3f, -0.5f);
-    glColor3ub(0,255,0);
-    glVertex2f(-0.3f, -0.5f);
-	glEnd();
-
-
-	//This is for Polygon
-	glBegin(GL_POLYGON);
-	glColor3ub(0,150,255);
-    glVertex2f(1.0f, -1.0f);
-    glVertex2f(1.0f, -0.5f);
-    glVertex2f(0.8f, -0.25f);
-    glVertex2f(0.6f, -0.5f);
-    glVertex2f(0.75f, -0.9f);
-    glVertex2f(0.75f, -1.0f);
-	glEnd();
 
     glFlush();
 }
@@ -77,18 +66,5 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
